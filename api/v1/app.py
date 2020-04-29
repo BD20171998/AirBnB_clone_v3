c48ec9eff3d928f76d9d991bdfde705b3888fced
Nehal Shastri <nhlshstr@gmail.com>
2020-04-29 15:38:29 -0700

    added app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-""" api """
+""" API """
 
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def tear_down(self):
     """tear down method"""
     storage.close()
 
+
 if __name__ == "__main__":
     if getenv('HBNB_API_HOST') is not None:
-        host = getenv('HBNB_TYPE_STORAGE')
+        host = getenv('HBNB_TYPE_HOST')
 
     else:
         host = '0.0.0.0'
 
     if getenv('HBNB_API_PORT') is not None:
-        port  = getenv('HBNB_API_PORT')
+        port = getenv('HBNB_API_PORT')
 
     else:
         port = 5000
 
-    threaded=True
+    app.run(host=host, port=port, threaded=True)
