853ccd01590814164bf8e9c32038de831fa66124
Nehal Shastri <nhlshstr@gmail.com>
2020-04-29 16:23:39 -0700

    return changed to var

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,9 @@
 from api.v1.views.__init__ import app_views
 from flask import Flask, render_template
 
+
 @app_views.route("/status", strict_slashes=False)
 def app_views():
     """create a route /status on the object app_views that returns a JSON:"""
-
-    return {"status": "OK"}
+    status_dict = {"status": "OK"}
+    return status_dict
