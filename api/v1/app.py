21c61007839d9c7fd5a4eba5c874b693193ae58b
Nehal Shastri <nhlshstr@gmail.com>
2020-04-30 14:41:21 -0700

    added error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ API """
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def tear_down(self):
     """tear down method"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Error 404"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
