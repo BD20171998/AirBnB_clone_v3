e9b084e794d9dcc337e5a4417ef812be28c60143
Nehal Shastri <nhlshstr@gmail.com>
2020-04-30 18:53:37 -0700

    added error handling, code returns

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """State object view """
 from flask import Flask, jsonify, request, Response
+from flask import abort
 from models.state import State
 from models import storage
 from api.v1.views import app_views
@@ -20,9 +21,13 @@ def states():
 def state_id(state_id):
     """Retrieves a State object based on id"""
     if state_id is not None:
-        single_state = storage.get(State, state_id)
+        single_state = storage.get("State", state_id)
+        if single_state is None:
+            abort(404)
         single_state_dict = single_state.to_dict()
         return jsonify(single_state_dict)
+    else:
+        abort(404)
 
 
 @app_views.route("/states/<state_id>", strict_slashes=False,
@@ -30,36 +35,47 @@ def state_id(state_id):
 def state_delete(state_id):
     """Deletes a state object"""
     if state_id is not None:
-        cities_del = storage.all("City")
-        for i in cities_del.values():
-            if state_id == getattr(i, "state_id"):
-                i.delete()
-                storage.save()
-        state_del = storage.get('State', state_id)
-        state_del.delete()
-        storage.save()
-        return {}
+        del_state = storage.get("State", state_id)
+        if del_state is None:
+            abort(404)
+        ret_del_state = del_state.to_dict()
+        return jsonify(ret_del_state)
+
+    else:
+        abort(404)
 
 
 @app_views.route("/states", strict_slashes=False, methods=['POST'])
 def state_add():
     """Adds a state object"""
     data = request.get_json()
+    if data is None:
+        err_return = {"error": "Not a JSON"}
+        return jsonify(err_return), 400
+    if "name" not in data:
+        err_return = {"error": "Missing name"}
+        return jsonify(err_return), 400
     new = State(**data)
-    new.save()
+    storage.new(new)
     storage.save()
-    status_code = Response(status=201)
-    return status_code
+    status_code = 201
+    new_state_dict = new.to_dict()
+    return jsonify(new_state_dict), status_code
 
 
 @app_views.route("/states/<state_id>", strict_slashes=False, methods=['PUT'])
 def state_update(state_id):
     """Update an existing state object"""
     data = request.get_json()
-    single_state = storage.get(State, state_id)
+    if data is None:
+        error_dict = {"error": "Not a JSON"}
+        return jsonify(error_dict), 400
+    single_state = storage.get("State", state_id)
+    if single_state is None:
+        abort(404)
 
     setattr(single_state, 'name', data['name'])
     single_state.save()
     storage.save()
 
-    return jsonify(single_state.to_dict())
+    return jsonify(single_state.to_dict()), 200
