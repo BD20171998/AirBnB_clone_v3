0b0d4fc58c801c3cd7ff434e7be9bce257cea1c8
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-01 00:19:02 +0000

    Added PUT method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,14 +4,18 @@ from flask import Flask, jsonify, request, Response
 from models.state import State
 from models import storage
 from api.v1.views import app_views
+
+
 @app_views.route("/states", strict_slashes=False,  methods=['GET'])
 def states():
     """retrieve State object(s)"""
     state_list = []
     all_states = storage.all(State)
-    for k,v in all_states.items():
+    for k, v in all_states.items():
         state_list.append(v.to_dict())
     return jsonify(state_list)
+
+
 @app_views.route("/states/<state_id>", strict_slashes=False, methods=['GET'])
 def state_id(state_id):
     """Retrieves a State object based on id"""
@@ -19,7 +23,10 @@ def state_id(state_id):
         single_state = storage.get(State, state_id)
         single_state_dict = single_state.to_dict()
         return jsonify(single_state_dict)
-@app_views.route("/states/<state_id>", strict_slashes=False, methods=['DELETE'])
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False,
+                 methods=['DELETE'])
 def state_delete(state_id):
     """Deletes a state object"""
     if state_id is not None:
@@ -32,6 +39,8 @@ def state_delete(state_id):
         state_del.delete()
         storage.save()
         return {}
+
+
 @app_views.route("/states", strict_slashes=False, methods=['POST'])
 def state_add():
     """Adds a state object"""
@@ -41,12 +50,16 @@ def state_add():
     storage.save()
     status_code = Response(status=201)
     return status_code
+
+
 @app_views.route("/states/<state_id>", strict_slashes=False, methods=['PUT'])
 def state_update(state_id):
     """Update an existing state object"""
     data = request.get_json()
     single_state = storage.get(State, state_id)
-    single_state(**data)
+
+    setattr(single_state, 'name', data['name'])
     single_state.save()
     storage.save()
-    new = single_state.to_dict()
+
+    return jsonify(single_state.to_dict())
